1f2e42eaa0700bea5b6550a3ed5ea158399e82a0
Yevhen_Pavlenko <Yevhen.Pavlenko@refinitiv.com>
2021-01-25 11:04:17 +0200

    Updated names

--- src/main/java/mergeUnsortedArrays.java
+++ src/main/java/mergeUnsortedArrays.java
@@ -1,37 +1,37 @@
 import java.util.Arrays;
 
-public class mergeSortedArrays {
+public class mergeUnsortedArrays {
 
-    public static int[] mergeSortedArrays(int[] arr1, int arr2[]) {
-        int[] mergeSortedArray = new int[arr1.length + arr2.length];
+    public static int[] mergeUnsortedArrays(int[] arr1, int arr2[]) {
+        int[] mergeUnsortedArray = new int[arr1.length + arr2.length];
 
         int i = 0, j = 0, k = 0;
 
         while (i < arr1.length && j < arr2.length) {
             if (arr1[i] < arr2[j]) {
-                mergeSortedArray[k++] = arr1[i++];
+                mergeUnsortedArray[k++] = arr1[i++];
             } else {
-                mergeSortedArray[k++] = arr2[j++];
+                mergeUnsortedArray[k++] = arr2[j++];
             }
         }
 
         while (i < arr1.length) {
-            mergeSortedArray[k++] = arr1[i++];
+            mergeUnsortedArray[k++] = arr1[i++];
         }
 
         while (j < arr2.length) {
-            mergeSortedArray[k++] = arr2[j++];
+            mergeUnsortedArray[k++] = arr2[j++];
         }
 
-        Arrays.sort(mergeSortedArray);
-        return mergeSortedArray;
+        Arrays.sort(mergeUnsortedArray);
+        return mergeUnsortedArray;
     }
 
 
     public static void main(String[] args) {
         int[] arr1 = {1, 2, 3, 4, 5};
         int[] arr2 = {9, 8, 7};
-        int[] mergeSortedArrays = mergeSortedArrays(arr1, arr2);
+        int[] mergeSortedArrays = mergeUnsortedArrays(arr1, arr2);
 
         System.out.println("Array1: " + Arrays.toString(arr1));
         System.out.println("Array2: " + Arrays.toString(arr2));
